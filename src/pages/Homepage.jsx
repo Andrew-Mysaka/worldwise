c363b9ee587f1ca91737bfd0f8ba9b492cdbd10c
Andrew-Mysaka <andrewmysaka@gmail.com>
2023-11-05 12:10:28 +0200

    Linking Between Routes With <Link/> and <NavLink/>

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,20 +1,22 @@
 import styles from "./Homepage.module.css";
+import PageNav from "../components/PageNav";
 
 export default function Homepage() {
-  return (
-    <main className={styles.homepage}>
-      <section>
-        <h1>
-          You travel the world.
-          <br />
-          WorldWise keeps track of your adventures.
-        </h1>
-        <h2>
-          A world map that tracks your footsteps into every city you can think
-          of. Never forget your wonderful experiences, and show your friends how
-          you have wandered the world.
-        </h2>
-      </section>
-    </main>
-  );
+    return (
+        <main className={styles.homepage}>
+            <PageNav/>
+            <section>
+                <h1>
+                    You travel the world.
+                    <br/>
+                    WorldWise keeps track of your adventures.
+                </h1>
+                <h2>
+                    A world map that tracks your footsteps into every city you can think
+                    of. Never forget your wonderful experiences, and show your friends how
+                    you have wandered the world.
+                </h2>
+            </section>
+        </main>
+    );
 }
--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,7 +1,10 @@
+import PageNav from "../components/PageNav";
+
 export default function PageNotFound() {
-  return (
-    <div>
-      <h1>Page not found 😢</h1>
-    </div>
-  );
+    return (
+        <div>
+            <PageNav/>
+            <h1>Page not found 😢</h1>
+        </div>
+    );
 }
