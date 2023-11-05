c5e5c7f3b3831722da94e53fbd9201a32486c605
Andrew-Mysaka <andrewmysaka@gmail.com>
2023-11-05 22:06:21 +0200

    Programmatic Navigation with <Navigate/>

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {BrowserRouter, Route, Routes, Navigate} from "react-router-dom";
 import {useEffect, useState} from "react";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
@@ -42,7 +42,7 @@ function App() {
                 <Route path={'pricing'} element={<Pricing/>}/>
                 <Route path={'login'} element={<Login/>}/>
                 <Route path={'app'} element={<AppLayout/>}>
-                    <Route index element={<CityList cities={cities} isLoading={isLoading}/>}/>
+                    <Route index element={<Navigate replace to={'cities'}/>}/>
                     <Route path={'cities'} element={<CityList cities={cities} isLoading={isLoading}/>}/>
                     <Route path={'cities/:id'} element={<City/>}/>
                     <Route path={'countries'} element={<CountryList cities={cities} isLoading={isLoading}/>}/>
