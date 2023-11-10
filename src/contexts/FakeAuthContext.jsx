9bfe5977c25bb13aed880122e7be0261e1959aeb
Andrew-Mysaka <andrewmysaka@gmail.com>
2023-11-10 21:21:29 +0200

    adding fake authentication: implementing "login"

--- src/contexts/FakeAuthContext.jsx
+++ src/contexts/FakeAuthContext.jsx
@@ -19,8 +19,8 @@ function reducer(state, action){
 }
 
 const FAKE_USER = {
-    name: "Jack",
-    email: "jack@example.com",
+    name: "Andrew",
+    email: "andrew@example.com",
     password: "qwerty",
     avatar: "https://i.pravatar.cc/100?u=zz",
 };
@@ -42,8 +42,8 @@ function AuthProvider({children}){
 
 function useAuth(){
     const context = useContext(AuthContext);
-
     if(context === undefined) throw new Error("AuthContext was used outside AuthProvider");
+    return context;
 }
 
 export {AuthProvider, useAuth};
--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import SideBar from "../components/SideBar";
 import style from './AppLayout.module.css';
 import Map from "../components/Map";
+import User from "../components/User.jsx";
 
 function AppLayout() {
     return (
         <div className={style.app}>
             <SideBar/>
             <Map/>
+            <User/>
         </div>
     );
 }
