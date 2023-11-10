5419a1f79670ae1782d805d582c345161d4d36fb
Andrew-Mysaka <andrewmysaka@gmail.com>
2023-11-10 21:58:29 +0200

    adding fake authentication: protecting a route

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import City from "./components/City";
 import Form from "./components/Form";
 import {CitiesProvider} from "./contexts/CitiesContext.jsx";
 import {AuthProvider} from "./contexts/FakeAuthContext.jsx";
+import ProtectedRoute from "./pages/ProtectedRoute.jsx";
 
 function App() {
     return (
@@ -22,7 +23,10 @@ function App() {
                         <Route path={'product'} element={<Product/>}/>
                         <Route path={'pricing'} element={<Pricing/>}/>
                         <Route path={'login'} element={<Login/>}/>
-                        <Route path={'app'} element={<AppLayout/>}>
+                        <Route path={'app'} element={
+                            <ProtectedRoute>
+                                <AppLayout/>
+                            </ProtectedRoute>}>
                             <Route index element={<Navigate replace to={'cities'}/>}/>
                             <Route path={'cities'} element={<CityList/>}/>
                             <Route path={'cities/:id'} element={<City/>}/>
---
+++ src/pages/ProtectedRoute.jsx
@@ -0,0 +1,16 @@
+import {useAuth} from "../contexts/FakeAuthContext.jsx";
+import {useNavigate} from "react-router-dom";
+import {useEffect} from "react";
+
+const ProtectedRoute = ({children}) => {
+    const {isAuthenticated} = useAuth();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if(!isAuthenticated) navigate("/");
+    }, [isAuthenticated, navigate]);
+
+    return isAuthenticated ? children : null;
+};
+
+export default ProtectedRoute;
